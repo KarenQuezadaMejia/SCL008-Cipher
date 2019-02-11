d2b8465b5e9cfb742ee1f43ff0bfa1b49fa8a426
KarenQuezadaMejia <mac@MacBook-Pro-de-mac.local>
2019-02-11 11:22:18 -0300

    se agrega funcion decode y encode de cipher, y pruebas en index.js con DOM

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,45 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+window.cipher => {
+
+	encode(offset,string){
+     let newMssag="";
+     let newMssag1 ="";
+     let result ="";
+
+     let mssag = string.toUpperCase();
+
+     for(let i=0; i<string.length; i++)
+     {
+         newMssag= mssag.charCodeAt(i);
+
+         if( newMssag>=65 && newMssag<=90)
+          {
+             newMssag1=(newMssag -65 +parseInt(offset))%26 + 65;
+             result += String.fromCharCode(newMssag1);
+
+           }
+         }
+           return result;
+		},
+
+ 
+  decode(offset,string){
+     let newMssag="";
+     let newMssag1 ="";
+     let result ="";
+
+     let mssag = string.toUpperCase();
+
+     for(let i=0; i<string.length; i++)
+     {
+         newMssag= mssag.charCodeAt(i);
+
+         if( newMssag>=65 && newMssag<=90)
+          {
+             newMssag1=(newMssag -90 -parseInt(offset))%26 + 90;
+             result += String.fromCharCode(newMssag1);
+
+           }
+         }
+           return result;
+		},
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 /* Acá va tu código */
+const btnName = document.getElementById
+('btn-name');
+
+btnName.addEventListener('click',() => {
+	const.inputName =document.getElementById('input-name');
+
+	let valueInput = inputName.value
+
+	console.log(valueInput);
+} )
