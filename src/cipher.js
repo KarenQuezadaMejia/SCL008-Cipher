f316b7a468ef83d96c2520a58cce15bbd6089133
KarenQuezadaMejia <mac@MacBook-Pro-de-mac.local>
2019-02-12 12:39:44 -0300

    Se finalizan pruebas de test, en spec.js, en js y en prueba de inicialización de redme

--- src/cipher.js
+++ src/cipher.js
@@ -42,8 +42,8 @@ window.cipher = {
 
  
   decode(offset,string){
-     let newMssag="";
-     let newMssag1 ="";
+     let newMssag=0;
+     let newMssag1 =0;
      let result ="";
 
      let mssag = string;
@@ -58,26 +58,26 @@ window.cipher = {
 
         else if( newMssag>=32 && newMssag<=64)
           {
-             newMssag1=(newMssag -32 -parseInt(offset))%33 + 32;
+             newMssag1=(newMssag -64 -parseInt(offset))%33 + 64;
              result += String.fromCharCode(newMssag1);
 
            }
 
           else if( newMssag>=97 && newMssag<=122)
           {
-             newMssag1=(newMssag -97 -parseInt(offset))%26 + 97;
+             newMssag1=(newMssag -122 -parseInt(offset))%26 + 122;
              result += String.fromCharCode(newMssag1);
 
            }
 
           else if( newMssag>=65 && newMssag<=90)
           {
-             newMssag1=(newMssag -65 -parseInt(offset))%26 + 65;
+             newMssag1=(newMssag -90 -parseInt(offset))%26 + 90;
              result += String.fromCharCode(newMssag1);
 
            }
 
          }
            return result;
-    }
+    },
 }
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,16 +10,24 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33');
+    it('debería retornar "wigi ymn3m2" para "como est@s?" con offset 20', () =>{
+    assert.equal(cipher.encode(20, "como est@s?"), "wigi ymn3m2");
+  });
+
+    it('debería retornar "XIrks 598 tvy7f8" para "TEngo 154 pru3b4" con offset 4', () =>{
+    assert.equal(cipher.encode(4, "TEngo 154 pru3b4"), "XIrks 598 tvy7f8");
+  });
+
+
     it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () =>{
     assert.equal(cipher.encode(33, "abcdefghijklmnopqrstuvwxyz"), "hijklmnopqrstuvwxyzabcdefg");
   });
     
     
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () =>{
     assert.equal(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
   });
+
   });
 
 
@@ -29,14 +37,20 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33');
-    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () =>{
-    assert.equal(cipher.decode("hijklmnopqrstuvwxyzabcdefg"), "abcdefghijklmnopqrstuvwxyz");
+    it('debería retornar "como est@s?" para "wigi ymn3m2" con offset 20', () =>{
+    assert.equal(cipher.decode(20, "wigi ymn3m2"), "como est@s?");
   });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+    it('debería retornar "TEngo 154 pru3b4" para "XIrks 598 tvy7f8" con offset 4', () =>{
+    assert.equal(cipher.decode(4, "XIrks 598 tvy7f8"), "TEngo 154 pru3b4");
+  });
+
+     it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () =>{
+    assert.equal(cipher.decode(33, "hijklmnopqrstuvwxyzabcdefg"),"abcdefghijklmnopqrstuvwxyz");
+  });
+
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () =>{
-      assert.equal(cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+      assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFg"), "ABCDEFGHIJKLMNOPQRSTUVWXYz");
     });
 
 
