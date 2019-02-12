d8b8723e70d586fb46c93218025d6015deb487ff
KarenQuezadaMejia <mac@MacBook-Pro-de-mac.local>
2019-02-12 20:23:30 -0300

    se agregan botones para funcionamiento de cifrado, se descargan imagen para mejora en css, al agregar botones se hicieron pruebas en indes.js ya que se modificaron eventos DOM, todo ok por fin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+
+const button_init = document.getElementById("clickInit");
+button_init.addEventListener('click',()=>{
+  document.getElementById('intro').style.display="none";
+  document.getElementById('container').style.display="block";
+
+});
+
 let final = "";
 
 const encode = document.getElementById("clickEncode");
+
 
 encode.addEventListener('click', () => {
    const skipNumber = document.getElementById("skipNumber");
@@ -26,6 +35,28 @@ decode.addEventListener('click', () => {
 
    document.getElementById('cipherContent').value=final;
 })
+
+
+const home = document.getElementById("retHome");
+home.addEventListener('click',()=>{
+  document.getElementById('intro').style.display="block";
+  document.getElementById('container').style.display="none";
+});
+
+const clean = document.getElementById("clear");
+clean.addEventListener('click',()=>{
+  document.getElementById('container').style.display="block";
+  document.getElementById('textMessage').value="";
+  document.getElementById('cipherContent').value="";
+  document.getElementById('skipNumber').value="";
+});
+
+const sending = document.getElementById("send");
+sending.addEventListener('click',()=>{
+  prompt('Ingresa tu e-mail para envío tu nueva clave')
+  alert('Clave enviada')
+});
+
 
 /*let final = "";
 
