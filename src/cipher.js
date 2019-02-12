2a5dcdde3b733f327bde4857d3ed5a4d8eedf991
KarenQuezadaMejia <mac@MacBook-Pro-de-mac.local>
2019-02-12 00:24:04 -0300

    pruebas para test decode

--- src/cipher.js
+++ src/cipher.js
@@ -56,28 +56,28 @@ window.cipher = {
           result+=String.fromCharCode(newMssag);
          }
 
-        else if( newMssag>32 && newMssag<=64)
+        else if( newMssag>=32 && newMssag<=64)
           {
-          newMssag1=(newMssag -32 -parseInt(offset))%33 + 32;
-          result += String.fromCharCode(newMssag1);
+             newMssag1=(newMssag -32 -parseInt(offset))%33 + 32;
+             result += String.fromCharCode(newMssag1);
 
            }
 
-          else if( newMssag>97 && newMssag<=122)
+          else if( newMssag>=97 && newMssag<=122)
           {
-            newMssag1=(newMssag -97 -parseInt(offset))%26 + 97;
-            result += String.fromCharCode(newMssag1);
+             newMssag1=(newMssag -97 -parseInt(offset))%26 + 97;
+             result += String.fromCharCode(newMssag1);
 
            }
 
-          else if( newMssag>65 && newMssag<=90)
+          else if( newMssag>=65 && newMssag<=90)
           {
-            newMssag1=(newMssag -65 +parseInt(offset))%26 + 65;
-            result += String.fromCharCode(newMssag1);
+             newMssag1=(newMssag -65 -parseInt(offset))%26 + 65;
+             result += String.fromCharCode(newMssag1);
 
            }
 
          }
            return result;
-    },
+    }
 }
