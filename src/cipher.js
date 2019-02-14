58b8b27d2e76b195002a53f89a2eed72f5f61365
KarenQuezadaMejia <mac@MacBook-Pro-de-mac.local>
2019-02-14 10:44:37 -0300

    comentarios

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,45 @@
 window.cipher = {
-
+/*para la función encode se tomaran valores de un offset y un string*/
   encode(offset,string){
+    /*se declaran variables de nweMessage, newmssag1, result en vacios porque guardaran
+    contenido de las futuras acciones*/
      let newMssag="";
      let newMssag1 ="";
      let result ="";
-
+     /*se crea una variable de mensaje que contendra el string*/
      let mssag = string;
-
+     /*se inicia indicando que el inicio sera 0, calculando el largo del string, sumando a 1*/
      for(let i=0; i<string.length; i++)
      {
+      /*en donde el nuevo mensaje me transformara a un valor ascii de acuerdo a la posición guardado en newmssage*/
          newMssag= mssag.charCodeAt(i);
-
+         /*si el nuevo mensaje es igual a 32 me va a retornar el mismo valor que es un espacio*/
         if (newMssag===32){
+          /*en donde el resultado me devolvera el valor sin formula para respetar el espacio*/
           result+=String.fromCharCode(newMssag);
          }
-
+         /*si el rango del valor esta entre 32 y 64*/
         else if( newMssag>=32 && newMssag<=64)
           {
+            /*Para números y caracteres*/
+            /*se declara el newmssge1 que contendra el mensaje, en donde el rrango si es menor a 32 sumando el offset
+            más el residuo y agregando 32 -formula michelle*/
              newMssag1=(newMssag -32 +parseInt(offset))%33 + 32;
+             /*da como resultado el string tramsformado al valor del ascci del mensaje guardado de acuerdo a la formula
+             en donde el mensaje se guardará en la variable result*/
              result += String.fromCharCode(newMssag1);
 
            }
-
+           /*Para minúsculas*/
+            /*se declara el newmssge1 que contendra el mensaje, en donde el rrango si es menor a 97 sumando el offset
+            más el residuo y agregando 97 -formula michelle*/
           else if( newMssag>=97 && newMssag<=122)
           {
              newMssag1=(newMssag -97 +parseInt(offset))%26 + 97;
              result += String.fromCharCode(newMssag1);
 
            }
-
+           /*Para mayuscúlas*/
           else if( newMssag>=65 && newMssag<=90)
           {
              newMssag1=(newMssag -65 +parseInt(offset))%26 + 65;
@@ -37,6 +48,8 @@ window.cipher = {
            }
 
          }
+         /*como final en el resultado retornará el valor guardado de acuerdo a la condición 
+         de los parametros ingresados, teniendo en cuenta que result inicio como variable con strings vacios */
            return result;
     },
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
-
+/* Se usa para declarar variable para dar función al boton de Comenzar */
 const button_init = document.getElementById("clickInit");
+/*Se llama a la acción del boton de comenzar*/
 button_init.addEventListener('click',()=>{
   document.getElementById('intro').style.display="none";
   document.getElementById('container').style.display="block";
 
 });
-
+/* se inicia declarando el valor final en vacio
+porque llenara con el resultado en la caja-cipher
+luego se llama crea la función para llamar del cipher.j al index.js*/
 let final = "";
 
+/* se declara la funcion del boton encode declarade en cipher.js*/
 const encode = document.getElementById("clickEncode");
 
-
+/* se llama a la funcion del boton encode declarade en cipher.js*/
 encode.addEventListener('click', () => {
+/* se declara las variables del offser y del mensaje*/
    const skipNumber = document.getElementById("skipNumber");
    const textMessage = document.getElementById("textMessage");
 
+/* se crean variables que contenga el número de offset y de mensaje sin cifras textMessage con el valor obtenido */
    let number= skipNumber.value;
    let message = textMessage.value;
    final= cipher.encode(number,message)
-
+/*Se declara el valor final para la caja cipher content para guardar el resultado de encode*/
    document.getElementById('cipherContent').value=final;
 })
+
+
+
 
 const decode = document.getElementById("clickDecode");
 decode.addEventListener('click', () => {
@@ -36,22 +45,32 @@ decode.addEventListener('click', () => {
    document.getElementById('cipherContent').value=final;
 })
 
-
+/*Para retornar al home*/
+/*se crea variable pata declarar la función del boton de regreso a home*/
 const home = document.getElementById("retHome");
+/*Se llama a la acción de la función de regresar a home*/
 home.addEventListener('click',()=>{
+  /*se declara que al presionar home retorne intro en html y el id container invisible*/
   document.getElementById('intro').style.display="block";
   document.getElementById('container').style.display="none";
 });
 
+/*Para borrar cajas de texto*/
 const clean = document.getElementById("clear");
+/*Se llamada a la accion del de la variable clean para la función de clear*/
 clean.addEventListener('click',()=>{
+  /*se declara que al hacer la función solo muestre la caja de container
+  eliminando el valor de los id textmessage, skipnumer, y ciphercontent*/
   document.getElementById('container').style.display="block";
   document.getElementById('textMessage').value="";
   document.getElementById('cipherContent').value="";
   document.getElementById('skipNumber').value="";
 });
 
+/*se declara boton para enviar e-mail*/
 const sending = document.getElementById("send");
+/*se indica que a la acción de la función se inicaría un promp solicitando un e-mail
+luego del alert un alert*/
 sending.addEventListener('click',()=>{
   prompt('Ingresa tu e-mail para envío tu nueva clave')
   alert('Clave enviada')
